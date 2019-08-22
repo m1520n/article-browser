2a18016475fcadea9e126ab64a650192bcac09c6
Michał Winiarski <m.winiarski.z@gmail.com>
2019-08-22 19:42:20 +0200

    Added prop-types

--- src/containers/Article/Article.js
+++ src/containers/Article/Article.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Article = () => <div>Article</div>;
+const propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
+
+const Article = ({ match }) => (
+  <div>
+Article
+    {match.params.id}
+  </div>
+);
+
+Article.propTypes = propTypes;
 
 export default Article;
