d39ed3d7a63f371df0c8cbd30b4fc5fedcf51aa1
Michał Winiarski <m.winiarski.z@gmail.com>
2019-08-22 12:43:48 +0200

    Added styled-normalize and styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Normalize } from 'styled-normalize';
+import styled from 'styled-components';
 
+const H1 = styled.h1`
+  color: green;
+`;
 const App = () => (
-  <div>
-    <h1> Hello, World! </h1>
-  </div>
+  <>
+    <Normalize />
+    <H1> Hello, World! </H1>
+  </>
 );
 
 export default App;
