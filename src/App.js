b6dd596f30519104190dae2d9173d95435f9fd11
Michał Winiarski <m.winiarski.z@gmail.com>
2019-08-22 19:34:20 +0200

    Added routing with basic routes structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Normalize } from 'styled-normalize';
-import styled from 'styled-components';
 
-const H1 = styled.h1`
-  color: green;
-`;
+import Home from './containers/Home/Home';
+import Article from './containers/Article/Article';
+import NotFound from './containers/NotFound/NotFound';
+
 const App = () => (
   <>
     <Normalize />
-    <H1> Hello, World! </H1>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/article/:id" component={Article} />
+        <Route path="*" component={NotFound} />
+      </Switch>
+    </Router>
   </>
 );
 
---
+++ src/containers/NotFound/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const NotFound = () => <div>Ooops! The page you requested was not found...</div>;
+
+export default NotFound;
